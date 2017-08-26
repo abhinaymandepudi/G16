11f8b1f099bce0aaaf81aec5293f92a2fcc6937e
Hanlin He <hanling.he@gmail.com>
2017-08-25 22:26:47 -0500

    Start code in short project 1. Define signatures.

---
+++ src/cs6301/g16/BFS.java
@@ -0,0 +1,32 @@
+/**
+ * <h1>Fall 2017 Short Project 1</h1>
+ * <p>
+ * Implement breadth-first search (BFS), and solve the problem of finding the diameter of a tree
+ * that works as follows: Run BFS, starting at an arbitrary node as root. Let u be a node at maximum
+ * distance from the root. Run BFS again, with u as the root. Output diameter: path from u to a node
+ * at maximum distance from u.
+ *
+ * @author Binhan Wang / Hanlin He / Zheng Gao
+ * @version 0.1
+ * @since 2017-08-25
+ */
+
+package cs6301.g16;
+
+import cs6301.g00.Graph;
+
+import java.util.LinkedList;
+
+public class BFS {
+
+    /**
+     * Return a longest path in g.  Algorithm is correct only if g is a tree.
+     * @param g
+     * @return
+     */
+    static LinkedList<Graph.Vertex> diameter(Graph g) {
+
+        return new LinkedList<>();
+
+    }
+}
---
+++ src/cs6301/g16/Sort.java
@@ -0,0 +1,49 @@
+/**
+ * <h1>Fall 2017 Short Project 1</h1>
+ * <p>
+ * Implement the merge sort algorithm on generic arrays and on an int array and compare their
+ * running times on arrays sizes from 1M-16M, and with an O(n^2) time algorithm, such as Insertion
+ * sort. Write a report with a table and/or chart showing the times for different sizes.
+ *
+ * @author Binhan Wang / Hanlin He / Zheng Gao
+ * @version 0.1
+ * @since 2017-08-25
+ */
+
+package cs6301.g16;
+
+public class Sort {
+
+    /**
+     * Sort generic array arr, using array tmp as buffer.
+     * @param arr
+     * @param tmp
+     * @param <T>
+     */
+    static<T extends Comparable<? super T>> void mergeSort(T[] arr, T[] tmp) {
+
+    }
+
+    /**
+     * Sort int array arr, using array tmp as buffer.
+     * @param arr
+     * @param tmp
+     */
+    static void mergeSort(int[] arr, int[] tmp) {
+
+    }
+
+    /**
+     * Insertion/Bubble sort generic array arr in place.
+     * @param arr
+     * @param <T>
+     */
+    static<T extends Comparable<? super T>> void nSquareSort(T[] arr) {
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
