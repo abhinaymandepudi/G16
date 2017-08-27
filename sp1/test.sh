d3ec7bc77b1996e07ab3306745328b1bf88c9841
Hanlin He <hanling.he@gmail.com>
2017-08-27 17:30:19 -0500

    Add test script.

---
+++ sp1/test.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+types=("phantom" "Generic merge sort." "Int merge sort." "Generic n square sort.")
+for type in 1 2 3
+do
+    echo ${types[$type]}
+    for size in {1..6}
+    do
+        echo -n $size ''
+        echo "Array size: $size million. " ${types[$type]} >> result.txt
+        java cs6301.g16.Sort $type $size >> result.txt
+    done
+    echo ''
+done
