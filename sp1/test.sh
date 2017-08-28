225a395798a0ca87ca357278efa134e4a0783a36
Hanlin He <hanling.he@gmail.com>
2017-08-27 21:06:06 -0500

    Expand test array size to 16M.

--- sp1/test.sh
+++ sp1/test.sh
@@ -3,7 +3,7 @@ types=("phantom" "Generic merge sort." "Int merge sort." "Generic n square sort.
 for type in 1 2 3
 do
     echo ${types[$type]}
-    for size in {1..6}
+    for size in {1..16}
     do
         echo -n $size ''
         echo "Array size: $size million. " ${types[$type]} >> result.txt
