09fa2b0d4edc48c3958fdc74f04c340aa51cf1b6
Hanlin He <hanling.he@gmail.com>
2017-09-01 14:09:46 -0500

    Update sample code.

--- sp1/src/cs6301/g00/Item.java
+++ sp1/src/cs6301/g00/Item.java
@@ -4,48 +4,36 @@
  */
 
 package cs6301.g00;
-
 public class Item implements Comparable<Item> {
-	private int element;
+    private int element;
 
-	public Item(int x) {
-		element = x;
+    Item(int x) { element = x; }
+
+    public int getItem() { return element; }
+
+    public void setItem(int x) { element = x; }
+
+    public int compareTo(Item another) {
+	if (this.element < another.element) { return -1; }
+	else if (this.element > another.element) { return 1; }
+	else return 0;
+    }
+
+    public String toString() { return Integer.toString(element); }
+
+    public static void main(String[] args) {
+	final int n = 10;
+	Item[] A = new Item[n];
+	for(int i=0; i<n; i++) {
+	    A[i] = new Item(i+1);
 	}
 
-	public int getItem() {
-		return element;
-	}
+	Item x = new Item(8);
 
-	public void setItem(int x) {
-		element = x;
-	}
-
-	public int compareTo(Item another) {
-		if (this.element < another.element) {
-			return -1;
-		} else if (this.element > another.element) {
-			return 1;
-		} else
-			return 0;
-	}
-
-	public String toString() {
-		return Integer.toString(element);
-	}
-
-	public static void main(String[] args) {
-		final int n = 10;
-		Item[] A = new Item[n];
-		for (int i = 0; i < n; i++) {
-			A[i] = new Item(i + 1);
-		}
-
-		Item x = new Item(8);
-
-		System.out.println("Binary search for 8: " + Search.binarySearch(A, 0, 9, x));
-	}
+	System.out.println("Binary search for 8: " + Search.binarySearch(A, 0, 9, x));
+    }
 }
 
-/*
- * Sample output: Binary search for 8: true
- */
+/* Sample output:
+Binary search for 8: true
+*/
