43d7c9682e35b0574ad6ac4bec19e1fe0a492049
Hanlin He <hanling.he@gmail.com>
2017-09-02 16:42:08 -0500

    Rename class.

--- sp2/src/cs6301/g16/ReverseSinglyLinkedList.java
+++
@@ -1,4 +0,0 @@
-package cs6301.g16;
-
-public class ReverseSinglyLinkedList {
-}
---
+++ sp2/src/cs6301/g16/ReversibleSinglyLinkedList.java
@@ -0,0 +1,22 @@
+/**
+ * <h1>Fall 2017 Short Project 2</h1>
+ * <p>
+ * P4: Write recursive and nonrecursive functions for the following tasks:
+ * <ol><li>reverse the order of elements of the SinglyLinkedList class, </li>
+ * <li>print the elements of the SinglyLinkedList class, in reverse order. </li></ol>
+ * Write the code and annotate it with proper loop invariants. Running time: O(n)
+ *
+ * @author Binhan Wang (bxw161330) / Hanlin He (hxh160630) / Zheng Gao (zxg170430)
+ * @version 1.0
+ * @since 2017-09-02
+ */
+package cs6301.g16;
+
+import cs6301.g00.SinglyLinkedList;
+
+public class ReversibleSinglyLinkedList extends SinglyLinkedList {
+    public static void reverseRecursive(SinglyLinkedList h) {
+//        h.iterator().
+
+    }
+}
