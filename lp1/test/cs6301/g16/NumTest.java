48afacb74c72602ea086ba2b74b9f5277325d324
Hanlin He <hanling.he@gmail.com>
2017-09-07 13:14:52 -0500

    Generate unit test class for Num.

---
+++ lp1/test/cs6301/g16/NumTest.java
@@ -0,0 +1,48 @@
+package cs6301.g16;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NumTest {
+    @Test
+    public void add() throws Exception {
+    }
+
+    @Test
+    public void subtract() throws Exception {
+    }
+
+    @Test
+    public void product() throws Exception {
+    }
+
+    @Test
+    public void power() throws Exception {
+    }
+
+    @Test
+    public void divide() throws Exception {
+    }
+
+    @Test
+    public void mod() throws Exception {
+    }
+
+    @Test
+    public void power1() throws Exception {
+    }
+
+    @Test
+    public void squareRoot() throws Exception {
+    }
+
+    @Test
+    public void compareTo() throws Exception {
+    }
+
+    @Test
+    public void toString() throws Exception {
+    }
+
+}
