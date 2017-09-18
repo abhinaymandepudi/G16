c41a3aeec62598568c05fd491e40a83b2cade4c4
Hans Wang <hans.b.wang@gmail.com>
2017-09-17 22:40:24 -0500

    Uncomment TestEulerian

--- sp3/src/cs6301/g16/TestEulerian.java
+++ sp3/src/cs6301/g16/TestEulerian.java
@@ -29,73 +29,73 @@ public class TestEulerian extends SCC {
         super(g);
     }
 
-//    public boolean isEulerian() {
-//
-//        // check in-degree and out-degree of each vertex
-//        for (Graph.Vertex v : g)
-//            if (v.inDegree() != v.outDegree())
-//                return false;
-//
-//        // check if the graph is strongly connected
-//        if (getStronglyConnectedComponents() != 1)
-//            return false;
-//
-//        return true;
-//    }
-//
-//    public static boolean testEulerian(Graph g) {
-//        TestEulerian TE = new TestEulerian(g);
-//        return TE.isEulerian();
-//    }
-//
-//    public static void main(String[] args) throws FileNotFoundException {
-//        /*
-//        Test Graph 1: (not Eulerian)
-//
-//        11 17
-//        1 11 1
-//        2 3 1
-//        2 7 1
-//        3 10 1
-//        4 1 1
-//        4 9 1
-//        5 4 1
-//        5 7 1
-//        5 8 1
-//        6 3 1
-//        7 8 1
-//        8 2 1
-//        9 11 1
-//        10 6 1
-//        11 3 1
-//        11 4 1
-//        11 6 1
-//
-//        Test Graph 2: (is Eulerian)
-//        3 3
-//        1 2 1
-//        2 3 1
-//        3 1 1
-//         */
-//
-//        Scanner in;
-//        if (args.length > 0) {
-//            File inputFile = new File(args[0]);
-//            in = new Scanner(inputFile);
-//        } else {
-//            in = new Scanner(System.in);
-//        }
-//        Graph g = Graph.readGraph(in, true);
-//
-//        System.out.println("\n==============\nInput Graph:");
-//        for (Graph.Vertex u : g) {
-//            System.out.print(u + ": ");
-//            for (Graph.Edge e : u)
-//                System.out.print(e + " ");
-//            System.out.println();
-//        }
-//
-//        System.out.println("\n==============\nWhether the graph is Eulerian: " + testEulerian(g));
-//
-//    }
+    public boolean isEulerian() {
+
+        // check in-degree and out-degree of each vertex
+        for (Graph.Vertex v : g)
+            if (v.inDegree() != v.outDegree())
+                return false;
+
+        // check if the graph is strongly connected
+        if (getStronglyConnectedComponents() != 1)
+            return false;
+
+        return true;
+    }
+
+    public static boolean testEulerian(Graph g) {
+        TestEulerian TE = new TestEulerian(g);
+        return TE.isEulerian();
+    }
+
+    public static void main(String[] args) throws FileNotFoundException {
+        /*
+        Test Graph 1: (not Eulerian)
+
+        11 17
+        1 11 1
+        2 3 1
+        2 7 1
+        3 10 1
+        4 1 1
+        4 9 1
+        5 4 1
+        5 7 1
+        5 8 1
+        6 3 1
+        7 8 1
+        8 2 1
+        9 11 1
+        10 6 1
+        11 3 1
+        11 4 1
+        11 6 1
+
+        Test Graph 2: (is Eulerian)
+        3 3
+        1 2 1
+        2 3 1
+        3 1 1
+         */
+
+        Scanner in;
+        if (args.length > 0) {
+            File inputFile = new File(args[0]);
+            in = new Scanner(inputFile);
+        } else {
+            in = new Scanner(System.in);
+        }
+        Graph g = Graph.readGraph(in, true);
+
+        System.out.println("\n==============\nInput Graph:");
+        for (Graph.Vertex u : g) {
+            System.out.print(u + ": ");
+            for (Graph.Edge e : u)
+                System.out.print(e + " ");
+            System.out.println();
+        }
+
+        System.out.println("\n==============\nWhether the graph is Eulerian: " + testEulerian(g));
+
+    }
 }
