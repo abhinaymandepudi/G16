129d8a09c08c63ba225e9b9100e32390e895127d
gaozheng <835070013@qq.com>
2017-09-30 15:49:13 -0500

    Add Comments

--- sp5/src/cs6301/g16/SelectAlgorithm.java
+++ sp5/src/cs6301/g16/SelectAlgorithm.java
@@ -20,14 +20,20 @@ import java.util.stream.Stream;
 
 
 public class SelectAlgorithm {
-    static final int bound = 50;
+    private static final int bound = 50;   //Threshold of insertion sort
 
-    //(a)Create a priority queue (max heap) of the n elements, and use remove() k times.
+    /**
+     * Create a priority queue (max heap) of the n elements, and use remove() k times.
+     *
+     * @param A Array to select.
+     * @param k most k largest we want to get.
+     * @return kth largest number in array A.
+     */
     public static int PriorityQueueSort1(int[] A, int k) {
         Queue<Integer> queue = new PriorityQueue<>(k, Collections.reverseOrder());   // Trans to max heap
         List<Integer> list = new ArrayList<>();
-        for (int j = 0; j < A.length; j++) {   //build max-heap
-            queue.offer(A[j]);
+        for (int j : A) {   //Initial max-heap
+            queue.offer(j);
         }
         for (int i = 1; i <= k; i++) {
             list.add(queue.remove());
@@ -35,20 +41,24 @@ public class SelectAlgorithm {
         return list.get(k - 1);
     }
 
-    //(b)Use a priority queue (min heap) of size k to keep track of the k largest elements seen so far, as you iterate over the array.
-    // TODO: 2017/9/29  怎么从Stream中取元素存到queue？为啥不对  下面那个q.add又可以？
+    /**
+     * Use a priority queue (min heap) of size k to keep track of the k largest elements seen so far, as you iterate over the array.
+     *
+     * @param A Integer stream A to be select.
+     * @param k most k largest we want to get.
+     * @return kth largest number in array A.
+     */
     public static int PriorityQueueSort2(Stream<Integer> A, int k) {
         Iterator<Integer> it = A.iterator();
         Queue<Integer> q = new PriorityQueue<>();  //New a min heap
-        for (int i = 1; i <= k; i++) {    //build min heap
+        for (int i = 1; i <= k; i++) {    //Initial min-heap
             if (it.hasNext()) {
-               // int temp= (int)it.next();
                 q.add(it.next());
             }
         }
         while (it.hasNext()) {
             int x = it.next();
-            if (q.peek()<x) {
+            if (q.peek() < x) {
                 q.remove();
                 q.add(x);
             }
@@ -56,7 +66,13 @@ public class SelectAlgorithm {
         return q.peek();
     }
 
-    //(c) Implement the O(n) algorithm for Select discussed in class.
+    /**
+     * Implement the O(n) algorithm for Select discussed in class.
+     *
+     * @param A Integer stream A to be select.
+     * @param k Most k largest we want to get.
+     * @return Subarray of A
+     */
 
     public static int[] Select(int[] A, int k) {
         int n = A.length;
@@ -67,13 +83,15 @@ public class SelectAlgorithm {
     }
 
     /**
-     * @param A   array name
-     * @param p   array start index
-     * @param n   array's length
-     * @param k   kth largest element need to find
+     * Selection sort to get kth largest element in array A[p,p+n-1]
+     *
+     * @param A array name
+     * @param p array start index
+     * @param n array's length
+     * @param k kth largest element need to find
      * @return the element we find
      */
-    public static  int Select(int[] A, int p, int n, int k) {
+    public static int Select(int[] A, int p, int n, int k) {
         int r = p + n - 1;
         if (n < bound) {
             insertion_srt(A, p, r);
@@ -90,14 +108,20 @@ public class SelectAlgorithm {
         }
     }
 
-
-    public static  void insertion_srt(int[] arr, int start, int l) {
+    /**
+     * Insertion sort of array arr[start...start+l-1]
+     *
+     * @param arr   Array to be sort
+     * @param start Start index of sort.
+     * @param l     Length of array need to be sort.
+     */
+    public static void insertion_srt(int[] arr, int start, int l) {
         int k;
         int key;
         for (int i = start + 1; i <= l; i++) {
             key = arr[i];
             k = i - 1;
-            while (k > start - 1 && arr[k]>key) {
+            while (k > start - 1 && arr[k] > key) {
                 arr[k + 1] = arr[k];
                 k--;
             }
@@ -105,17 +129,17 @@ public class SelectAlgorithm {
         }
     }
 
-
-
-
+    /**
+     * Main test function.
+     */
     public static void main(String[] args) {
-        int[] item = new int[3000];
+        int[] item = new int[30000000];
         System.out.println("Input K(Kth largest element) of array (1-array.length):");
         Scanner scanner = new Scanner(System.in);
         int k = scanner.nextInt();
-        while(k<=0||k>item.length){
+        while (k <= 0 || k > item.length) {
             System.out.println("Input K(Kth largest element) of array (1-array.length):");
-            k=scanner.nextInt();
+            k = scanner.nextInt();
         }
         for (int i = 0; i < item.length; i++) {
             item[i] = i;
@@ -127,8 +151,9 @@ public class SelectAlgorithm {
         System.out.println("Kth element is: " + resA + "     Running time : " + (System.currentTimeMillis() - startTime) + "ms");
 
         System.out.println("test (b):");
-        ArrayList<Integer> a = new ArrayList<>(4000);
-        for (int i : item) a.add(i);
+        ArrayList<Integer> a = new ArrayList<>(40000000);
+        for (int i : item)
+            a.add(i);
         startTime = System.currentTimeMillis();
         int resB = PriorityQueueSort2(a.stream(), k);
         System.out.println("Kth element is: " + resB + "     Running time : " + (System.currentTimeMillis() - startTime) + "ms");
