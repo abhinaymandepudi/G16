29d5ed68c76b57b785f5a155a4f3a3897cc2fbaf
Hanlin He <hanling.he@gmail.com>
2017-10-04 17:14:48 -0500

    Implemented binary heap.

--- sp6/src/cs6301/g16/BinaryHeap.java
+++ sp6/src/cs6301/g16/BinaryHeap.java
@@ -1,59 +1,109 @@
 // Ver 1.0:  Starter code for bounded size Binary Heap implementation
 
 package cs6301.g16;
+
+import java.util.Arrays;
 import java.util.Comparator;
+import java.util.Random;
 
 public class BinaryHeap<T> {
     T[] pq;
-    Comparator<T> c;
-    /** Build a priority queue with a given array q, using q[0..n-1].
-     *  It is not necessary that n == q.length.
-     *  Extra space available can be used to add new elements.
+    final Comparator<T> c;
+    final int MAX_Size;
+    int size;
+
+    /**
+     * Build a priority queue with a given array q, using q[0..n-1]. It is not necessary that n ==
+     * q.length. Extra space available can be used to add new elements.
      */
     public BinaryHeap(T[] q, Comparator<T> comp, int n) {
-	pq = q;
-	c = comp;
+        pq = q;
+        c = comp;
+        MAX_Size = n;
+        size = 0;
     }
 
-    public void insert(T x) {
-	add(x);
+    public void insert(T x) throws Exception {
+        add(x);
     }
 
-    public T deleteMin() {
-	return remove();
+    public T deleteMin() throws Exception {
+        return remove();
     }
 
-    public T min() { 
-	return peek();
+    public T min() throws Exception {
+        return peek();
     }
 
-    public void add(T x) { /* TO DO. Throw exception if q is full. */
+    public void add(T x) throws Exception { /* TO DO. Throw exception if q is full. */
+        if (size == MAX_Size)
+            throw new Exception("The priority queue is full.");
+
+        pq[size] = x;
+        percolateUp(size);
+        size++;
     }
 
-    public T remove() { /* TO DO. Throw exception if q is empty. */
-	return null;
+    public T remove() throws Exception { /* TO DO. Throw exception if q is empty. */
+        if (size == 0)
+            throw new Exception("Priority queue is empty.");
+        T min = pq[0];
+        size--;
+        pq[0] = pq[size];
+        percolateDown(0);
+        return min;
     }
 
-    public T peek() { /* TO DO. Throw exception if q is empty. */
-	return null;
+    public T peek() throws Exception { /* TO DO. Throw exception if q is empty. */
+        if (size == 0)
+            throw new Exception("Priority queue is empty.");
+        return pq[0];
     }
 
     public void replace(T x) {
-	/* TO DO.  Replaces root of binary heap by x if x has higher priority
-	     (smaller) than root, and restore heap order.  Otherwise do nothing. 
+    /* TO DO.  Replaces root of binary heap by x if x has higher priority
+         (smaller) than root, and restore heap order.  Otherwise do nothing.
 	   This operation is used in finding largest k elements in a stream.
 	 */
     }
 
-    /** pq[i] may violate heap order with parent */
+    /**
+     * pq[i] may violate heap order with parent
+     */
     void percolateUp(int i) { /* to be implemented */
+        T tmp = pq[i];
+        while (i > 0 && c.compare(tmp, pq[parent(i)]) < 0) {
+            pq[i] = pq[parent(i)];
+            i = parent(i);
+        }
+        pq[i] = tmp;
     }
 
-    /** pq[i] may violate heap order with children */
+    /**
+     * pq[i] may violate heap order with children
+     */
     void percolateDown(int i) { /* to be implemented */
+        T tmp = pq[i];
+        int child = 2 * i + 1;
+        while (child <= size - 1) {
+            if (child < size - 1 && c.compare(pq[child], pq[child + 1]) > 0)
+                child++;
+            if (c.compare(tmp, pq[child]) <= 0)
+                break;
+            pq[i] = pq[child];
+            i = child;
+            child = 2 * i + 1;
+        }
+        pq[i] = tmp;
     }
 
-    /** Create a heap.  Precondition: none. */
+    int parent(int i) {
+        return (i - 1) / 2;
+    }
+
+    /**
+     * Create a heap.  Precondition: none.
+     */
     void buildHeap() {
     }
 
@@ -62,6 +112,32 @@ public class BinaryHeap<T> {
        min heap ==> descending order
        max heap ==> ascending order
      */
-    public static<T> void heapSort(T[] A, Comparator<T> comp) { /* to be implemented */
+    public static <T> void heapSort(T[] A, Comparator<T> comp) { /* to be implemented */
+    }
+
+    @Override
+    public String toString() {
+        return "[" + Arrays.toString(pq) + "]";
+    }
+
+    public static void main(String[] args) throws Exception {
+        Integer[] A = new Integer[100];
+        BinaryHeap<Integer> bh = new BinaryHeap<>(A, Comparator.naturalOrder(), 50);
+
+        Random r = new Random();
+        for (int i = 0; i < 50; i++)
+            bh.add(r.nextInt(100));
+
+
+        while (true) {
+            try {
+                System.out.println(bh.deleteMin());
+            } catch (Exception e){
+                break;
+            }
+        }
+
+        System.out.print(bh);
+
     }
 }
