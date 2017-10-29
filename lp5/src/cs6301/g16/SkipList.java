e43e03d81aac816f39674307e1d941db70596e04
gaozheng <835070013@qq.com>
2017-10-29 10:38:25 -0500

    Update lp5 add() remove()

--- lp5/src/cs6301/g16/SkipList.java
+++ lp5/src/cs6301/g16/SkipList.java
@@ -2,73 +2,189 @@
 // Change this to your group number
 package cs6301.g16;
 
-import cs6301.g00.*;
+
 import java.util.Iterator;
+import java.util.Random;
 
 // Skeleton for skip list implementation.
 
 public class SkipList<T extends Comparable<? super T>> {
+    private static int maxLevel = 32;
+
+    static class Entry<T> {
+        T element;
+        Entry[] next;
+        int level;
+
+        Entry(T ele, int lev) {
+            element = ele;
+            level = lev;
+            next = new Entry[lev+1];
+        }
+    }
+
+    private Entry<T> head;
+    private Entry<T> tail;
+    private Entry[] prev;
+    private int size;
+
+
     // Constructor
     public SkipList() {
+        head = new Entry<>(null, maxLevel);   //positive negative infinity?
+        tail = new Entry<>(null, maxLevel);
+        for (int i = 0; i < maxLevel; i++) {
+            head.next[i] = tail;
+        }
+        prev = new Entry[maxLevel];
+        size = 0;
+    }
+
+    //helper function
+    private Entry[] find(T x) {
+        Entry p = head;
+        for (int i = maxLevel - 1; i >= 0; i--) {
+            while (p.next[i].element != null &&
+                    x.compareTo((T) p.next[i].element) > 0) {
+                p = p.next[i];
+            }
+            prev[i] = p;
+        }
+        return prev;
+    }
+
+    private int chooseLevel(int lev) {
+        int i = 0;
+        while (i < lev) {
+            Random random = new Random();
+            boolean b = random.nextBoolean();
+            if (b) {
+                i++;
+            } else {
+                break;
+            }
+        }
+        return i;
     }
 
     // Add x to list. If x already exists, replace it. Returns true if new node is added to list
     public boolean add(T x) {
-	return true;
+        prev = new Entry[maxLevel];
+        prev = find(x);
+        if (prev[0].next[0] != null && prev[0].next[0].element == x) {
+            prev[0].next[0].element = x;
+        } else {
+            int lev = chooseLevel(maxLevel);
+            Entry n = new Entry<>(x, lev);
+            for (int i = 0; i <= lev; i++) {
+                n.next[i] = prev[i].next[i];
+                prev[i].next[i] = n;
+            }
+            size++;
+        }
+
+        return true;
     }
 
     // Find smallest element that is greater or equal to x
     public T ceiling(T x) {
-	return null;
+        return null;
     }
 
     // Does list contain x?
     public boolean contains(T x) {
-	return false;
+        prev = find(x);
+        return prev[0].next[0].element == x;
     }
 
     // Return first element of list
     public T first() {
-	return null;
+        return null;
     }
 
     // Find largest element that is less than or equal to x
     public T floor(T x) {
-	return null;
+        return null;
     }
 
     // Return element at index n of list.  First element is at index 0.
     public T get(int n) {
-	return null;
+        return null;
     }
 
     // Is the list empty?
     public boolean isEmpty() {
-	return false;
+        return false;
     }
 
     // Iterate through the elements of list in sorted order
     public Iterator<T> iterator() {
-	return null;
+        return null;
     }
 
     // Return last element of list
     public T last() {
-	return null;
+        return null;
     }
 
     // Reorganize the elements of the list into a perfect skip list
     public void rebuild() {
-	
+
     }
 
     // Remove x from list.  Removed element is returned. Return null if x not in list
     public T remove(T x) {
-	return null;
+        prev = find(x);
+        Entry n = prev[0].next[0];
+        if (n.element != x)
+            return null;
+        else {
+
+            for (int i = 0; i <= maxLevel; i++) {
+                if (prev[i].next[i].element == x) {
+                    prev[i].next[i] = n.next[i];
+                } else
+                    break;
+            }
+        }
+        size--;
+        return (T) n.element;
     }
 
     // Return the number of elements in the list
     public int size() {
-	return 0;
+        return size;
+    }
+
+    public void printList() {
+        Entry node = head.next[0];
+        System.out.println("----------START----------");
+        while (node != null && node.element != null) {
+            for (int i = 0; i < node.level + 1; i++) {
+                System.out.print(node.element);
+            }
+            for (int j = node.level + 1; j < maxLevel; j++) {
+                System.out.print("-");
+            }
+            System.out.println();
+            node = node.next[0];
+        }
+        System.out.println("----------END----------");
+    }
+
+    public static void main(String[] args) {
+        SkipList skipList = new SkipList();
+        skipList.add(3);
+        skipList.add(6);
+        skipList.add(5);
+        skipList.add(9);
+        skipList.add(7);
+        skipList.add(4);
+        skipList.add(8);
+        skipList.printList();
+        skipList.remove(3);
+        skipList.remove(5);
+        skipList.printList();//remove没成功！！
     }
 }
+
