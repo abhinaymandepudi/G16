c5a2478ca2accbfec90d48d7c271a7364bdb8c1a
Hanlin He <hanling.he@gmail.com>
2017-11-12 18:27:09 -0600

    Revert "Move timer to g16."
    This reverts commit 8e31efdc7f66245c31b9d2bd12cf1958c0f39e55.

--- lp4/src/cs6301/g00/Timer.java
+++ lp4/src/cs6301/g00/Timer.java
@@ -8,7 +8,7 @@
  *  Corrected memory calculations to 1048576 instead of 1000000
  */
 
-package cs6301.g16;
+package cs6301.g00;
 public class Timer {
     long startTime, endTime, elapsedTime, memAvailable, memUsed;
 
